1f1d3a1f0fe21dab434f7026fdb79c8cb0f53f63
Gabriel <gabriel.lazari@outlook.com>
2021-10-07 14:00:43 -0300

    static files

--- blog/settings.py
+++ blog/settings.py
@@ -133,6 +133,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 # STATICFILES_DIRS = [str(BASE_DIR / 'static')],
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'templates/static')]
+STATIC_ROOT = BASE_DIR / 'static'
 
 # MEDIA_ROOT = [str(BASE_DIR / 'media')],
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
